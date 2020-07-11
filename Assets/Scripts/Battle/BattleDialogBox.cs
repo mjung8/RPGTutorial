22c37d04194d551cd38024fd962876775c9380ca
mjung8 <mmkjung@gmail.com>
2020-07-10 23:19:05 -0400

    Add action and move selection

---
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogBox : MonoBehaviour
+{
+    [SerializeField]
+    private int lettersPerSecond;
+
+    [SerializeField]
+    private Color highlightedColor;
+
+    [SerializeField]
+    private Text dialogText;
+
+    [SerializeField]
+    private GameObject actionSelector;
+
+    [SerializeField]
+    private GameObject moveSelector;
+
+    [SerializeField]
+    private GameObject moveDetails;
+
+    [SerializeField]
+    private List<Text> actionTexts;
+
+    [SerializeField]
+    private List<Text> moveTexts;
+
+    [SerializeField]
+    private Text ppText;
+
+    [SerializeField]
+    private Text typeText;
+
+
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;
+    }
+
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for (int i = 0; i < actionTexts.Count; i++)
+        {
+            if (i == selectedAction)
+                actionTexts[i].color = highlightedColor;
+            else
+                actionTexts[i].color = Color.black;
+        }
+    }
+
+    public void UpdateMoveSelection(int selectedMove, Move move)
+    {
+        for (int i = 0; i < moveTexts.Count; i++)
+        {
+            if (i == selectedMove)
+                moveTexts[i].color = highlightedColor;
+            else
+                moveTexts[i].color = Color.black;
+        }
+
+        ppText.text = $"PP {move.PP}/{move.MoveBase.PP}";
+        typeText.text = move.MoveBase.Type.ToString();
+    }
+
+    public void SetMoveNames(List<Move> moves)
+    {
+        for (int i = 0; i < moveTexts.Count; i++)
+        {
+            if (i < moves.Count)
+                moveTexts[i].text = moves[i].MoveBase.Name;
+            else
+                moveTexts[i].text = "-";
+        }
+    }
+
+}
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy}
+
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField]
@@ -16,17 +18,114 @@ public class BattleSystem : MonoBehaviour
     [SerializeField]
     private BattleHud playerHud;
 
+    [SerializeField]
+    private BattleDialogBox dialogBox;
+
+    private BattleState state;
+
+    private int currentAction;
+    private int currentMove;
+
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    private void Update()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+        else if (state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+
+    public IEnumerator SetupBattle()
     {
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Pokemon);
 
         playerUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
+
+        dialogBox.SetMoveNames(playerUnit.Pokemon.Moves);
+
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.PokemonBase.Name} appeared.");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
     }
+
+    private void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action."));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    private void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+    private void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentAction < 1)
+                currentAction++;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction > 0)
+                currentAction--;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                // Fight
+                PlayerMove();
+            }
+            else if (currentAction == 1)
+            {
+                // Run
+            }
+        }
+    }
+
+    private void HandleMoveSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count - 1)
+                currentMove++;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentMove > 0)
+                currentMove--;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count - 2)
+                currentMove += 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentMove > 1)
+                currentMove -= 2;
+        }
+
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Pokemon.Moves[currentMove]);
+    }
+
 }
